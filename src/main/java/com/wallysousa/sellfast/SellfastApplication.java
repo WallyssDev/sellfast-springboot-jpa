9fb51b65d59b410f4efaeb514ededdcd2712fbad
Hebert Wallace <wally.dev@hotmail.com>
2026-03-09 18:39:07 -0300

    Project created

---
+++ src/main/java/com/wallysousa/sellfast/SellfastApplication.java
@@ -0,0 +1,13 @@
+package com.wallysousa.sellfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellfastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellfastApplication.class, args);
+	}
+
+}
