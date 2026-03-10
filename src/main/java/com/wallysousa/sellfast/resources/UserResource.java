b30359ac50831657798ba03600a5ee31a128a713
Hebert Wallace <wally.dev@hotmail.com>
2026-03-10 13:34:47 -0300

    User entity and resouce

---
+++ src/main/java/com/wallysousa/sellfast/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.wallysousa.sellfast.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wallysousa.sellfast.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "21981675156", "senha");
+		return ResponseEntity.ok().body(u);
+	}
+}
