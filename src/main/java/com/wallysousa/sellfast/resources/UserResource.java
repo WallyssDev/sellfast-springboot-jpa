29f31cc7f36da8c08ac1b007f433b2cc71355c80
Hebert Wallace <wally.dev@hotmail.com>
2026-03-10 18:32:10 -0300

    Service layer, component registration

--- src/main/java/com/wallysousa/sellfast/resources/UserResource.java
+++ src/main/java/com/wallysousa/sellfast/resources/UserResource.java
@@ -1,19 +1,31 @@
 package com.wallysousa.sellfast.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.wallysousa.sellfast.entities.User;
+import com.wallysousa.sellfast.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "21981675156", "senha");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
