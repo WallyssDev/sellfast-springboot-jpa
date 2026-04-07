4e92411406404e087b3593b8e56e16ff228ecddd
Hebert Wallace <wally.dev@hotmail.com>
2026-04-07 18:57:42 -0300

    Exception handling - findById

--- src/main/java/com/wallysousa/sellfast/services/UserService.java
+++ src/main/java/com/wallysousa/sellfast/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.wallysousa.sellfast.entities.User;
 import com.wallysousa.sellfast.repositories.UserRepository;
+import com.wallysousa.sellfast.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
